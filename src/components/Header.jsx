fb267070e26f7c7d5ecd73b39540493dbed8e9ef
soniagkalliath <soniageorge111@gmail.com>
2026-03-30 11:22:46 +0530

    redux with api call

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 import { Navbar,Nav,Container,Badge } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
-function Header() {
+function Header({insideProducts}) {
   return (
    <Navbar expand="lg" className="bg-primary fixed-top">
       <Container>
@@ -13,7 +13,7 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-           <Nav.Item> <input type="text" className="form-control" placeholder='Search Products Here!!!' /> </Nav.Item>
+           { insideProducts && <Nav.Item> <input type="text" className="form-control" placeholder='Search Products Here!!!' /> </Nav.Item>}
            <Link to={'/wishlist'} className='text-light text-decoration-none fw-bold'> <FontAwesomeIcon icon={faHeart} className='btn text-danger'/>  Wishlist <Badge pill bg="secondary">10</Badge> </Link>
             <Link to={'/cart'} className='text-light text-decoration-none fw-bold'> <FontAwesomeIcon icon={faCartPlus} className='btn text-success'/>  Cart <Badge pill bg="secondary">10</Badge> </Link>
           </Nav>
---
+++ src/redux/slices/productSlice.js
@@ -0,0 +1,41 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+//asynchorus action - should be dispatch by Product component
+export const getAllProducts = createAsyncThunk("products/getAllProducts",async ()=>{
+const result = await axios.get("https://dummyjson.com/products")
+// console.log(result.data.products);
+return result.data.products
+})
+
+const productSlice = createSlice({
+    name:'products',
+    initialState:{
+        loading:true,
+        allProducts:[],
+        error:""
+    },
+    reducers:{
+        //resolve only synchronous actions
+    },
+    extraReducers:(builder)=>{
+        // it resolve asynchronous action , Add reducers for additional action types here, and handle loading state as needed
+        builder.addCase(getAllProducts.fulfilled,(state,action)=>{
+            state.allProducts = action.payload
+            state.loading = false
+            state.error = ""
+        })
+        builder.addCase(getAllProducts.pending,(state,action)=>{
+            state.allProducts = []
+            state.loading = true
+            state.error = ""
+        })
+        builder.addCase(getAllProducts.rejected,(state,action)=>{
+            state.allProducts = []
+            state.loading = false
+            state.error = "API Call failed"
+        })
+    }
+})
+
+export default productSlice.reducer
